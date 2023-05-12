3cd68e07a26d1990ae8f756bba8c6717c43d5f73
kei-s <kei.shiratsuchi@gmail.com>
2023-05-12 16:42:44 +0900

    Remove unused

--- src/openAlternateFile.ts
+++ src/openAlternateFile.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-import * as path from 'path';
 import { showOrQuickOpen, getEditorInfo, EditorInfoError } from './util';
 
 export async function openAlternateFile() {
